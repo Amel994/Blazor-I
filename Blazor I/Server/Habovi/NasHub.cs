fd7036c9628eabbf9de66b2bd5e15b9006d4d04e
Amel Beganovic <beganovic_amel@LIVE.COM>
2020-06-26 19:37:30 +0200

    Add project files.

---
+++ Blazor I/Server/Habovi/NasHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_I.Server.Habovi
+{
+    public class NasHub : Hub
+    {
+        public void TestMetoda(string ulaz)
+        {
+            Console.WriteLine("Od klijenta: " + ulaz);
+            Clients.Caller.SendAsync("KlijentMetoda","Evo poruke od servera");
+        }
+    }
+}
